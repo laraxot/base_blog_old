dda96b1afd4965e6db008391781dd772d2c20c18
marco76tv <marco.sottana@gmail.com>
2018-11-07 16:42:02 +0100

    up

--- laravel/config/auth.php
+++ laravel/config/auth.php
@@ -67,7 +67,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            //'model' => App\User::class,
+            'model' => XRA\LU\Models\User::class,
         ],
 
         // 'users' => [
@@ -96,6 +97,8 @@ return [
             'provider' => 'users',
             'table' => 'password_resets',
             'expire' => 60,
+            'email' => 'lu::auth.emails.password',
+            'connection' => 'liveuser_general',
         ],
     ],
 
