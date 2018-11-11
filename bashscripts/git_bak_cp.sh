4fdcc5014add8348214d319c8e122c79113db475
marco76tv <marco.sottana@gmail.com>
2018-11-11 13:18:45 +0100

    backup con giorno

--- bashscripts/git_bak_cp.sh
+++ bashscripts/git_bak_cp.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cp -uvR ../blogx/laravel/packages ../_old4/blogx/laravel
-cp -uvR ../blogx/public_html/themes ../_old4/blogx/public_html
+day=$(date +%Y-%m-%d)
+curr_dir=${PWD##*/}
+bak_dir=../_backup/$day/$curr_dir
+echo $bak_dir
+mkdir -p $bak_dir/laravel
+mkdir -p $bak_dir/public_html
+cp -uvR ../$curr_dir/laravel/packages $bak_dir/laravel
+cp -uvR ../$curr_dir/public_html/themes $bak_dir/public_html
+
 
--- bashscripts/git_bak_sync.sh
+++ bashscripts/git_bak_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -aux --exclude '.git' --exclude '_old3' --exclude '_old4' --exclude '_OLD2' ../ ../_old4
+rsync -aux --exclude '.git' --exclude '_old3' --exclude '_OLD2' ../ ../_old3
 chmod -R 777 /var/www
