7b52ad5b6d3500b5d29c9d5fc695e4b7a2b81b73
marco76tv <marco.sottana@gmail.com>
2018-12-03 17:10:10 +0100

    .

--- laravel/config/localhost/xra.php
+++ laravel/config/localhost/xra.php
@@ -4,9 +4,10 @@ return [
     //'pub_theme'=>'amp_simpleblog',
     'adm_theme'=>'adminlte',
 	'model' => [
-        //'blog'          => \XRA\Blog\Models\Post::class,  //-------- TEST
-        'post'          => \XRA\Blog\Models\Post::class,  //-------- TEST
-        'postCat' 		=> \XRA\Blog\Models\PostCat::class,
+        //'post'          => \XRA\Blog\Models\Post::class,  //-------- TEST
+        //'postCat' 		=> \XRA\Blog\Models\PostCat::class,
+        'article'       => \XRA\Blog\Models\Article::class,  //-------- TEST
+        'articleCat' 	=> \XRA\Blog\Models\ArticleCat::class,
 		'home'          => \XRA\Blog\Models\Post::class,  //-------- TEST
 		'page'          => \XRA\Blog\Models\Page::class,
 		'feed'          => \XRA\Blog\Models\Feed::class,
@@ -15,4 +16,10 @@ return [
 	'migrate_packs'=>['xra','blog','lu','backend'],
 	'enable_packs'=>['frontend','backend','lu','blog','extend','xra','settings'],
 	'primary_lang' => 'it',
+
+	'related'=>[
+		'articleCat'=>['article'],
+		'article' =>[],
+	],
+
 ];
