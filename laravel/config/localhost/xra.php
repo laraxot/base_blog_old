c57a1a4f4965a6335ab4df366307acce7e5eedba
marco xot <marco.sottana@gmail.com>
2018-12-04 12:44:04 +0100

    .

--- laravel/config/localhost/xra.php
+++ laravel/config/localhost/xra.php
@@ -1,25 +1,35 @@
 <?php
-return [
-	'pub_theme'=>'minimal_blog',
-    //'pub_theme'=>'amp_simpleblog',
-    'adm_theme'=>'adminlte',
-	'model' => [
-        //'post'          => \XRA\Blog\Models\Post::class,  //-------- TEST
-        //'postCat' 		=> \XRA\Blog\Models\PostCat::class,
-        'article'       => \XRA\Blog\Models\Article::class,  //-------- TEST
-        'articleCat' 	=> \XRA\Blog\Models\ArticleCat::class,
-		'home'          => \XRA\Blog\Models\Post::class,  //-------- TEST
-		'page'          => \XRA\Blog\Models\Page::class,
-		'feed'          => \XRA\Blog\Models\Feed::class,
-        'sitemap'       => \XRA\Blog\Models\Sitemap::class,
-	],
-	'migrate_packs'=>['xra','blog','lu','backend'],
-	'enable_packs'=>['frontend','backend','lu','blog','extend','xra','settings'],
-	'primary_lang' => 'it',
-
-	'related'=>[
-		'articleCat'=>['article'],
-		'article' =>[],
-	],
-
-];
+return array(
+    'pub_theme' => 'amp_simpleblog',
+    'adm_theme' => 'adminlte',
+    'model' => array(
+        'article' => 'XRA\Blog\Models\Article',
+        'articleCat' => 'XRA\Blog\Models\ArticleCat',
+        'home' => 'XRA\Blog\Models\Post',
+        'page' => 'XRA\Blog\Models\Page',
+        'feed' => 'XRA\Blog\Models\Feed',
+        'sitemap' => 'XRA\Blog\Models\Sitemap',
+    ),
+    'migrate_packs' => array(
+        0 => 'xra',
+        1 => 'blog',
+        2 => 'lu',
+        3 => 'backend',
+    ),
+    'enable_packs' => array(
+        0 => 'frontend',
+        1 => 'backend',
+        2 => 'lu',
+        3 => 'blog',
+        4 => 'extend',
+        5 => 'xra',
+        6 => 'settings',
+    ),
+    'primary_lang' => 'it',
+    'related' => array(
+        'articleCat' => array(
+            0 => 'article',
+        ),
+        'article' => array(),
+    ),
+);
